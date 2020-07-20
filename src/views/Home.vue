fc63c810f58f7d54ec4bc06bd05dbe77a45d8029
mazayastudio <mazayastudio@hotmail.com>
2020-07-20 19:07:36 +0700

    Component Woman Banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div class="home">
     <HeaderShayna />
     <HeroShayna />
+    <WomenBanner />
   </div>
 </template>
 
 <script>
-import HeaderShayna from "./../components/HeaderShayna.vue";
-import HeroShayna from "./../components/HeroShayna.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomenBanner from "@/components/WomenBanner.vue";
 
 export default {
   name: "Home",
   components: {
     HeaderShayna,
     HeroShayna,
+    WomenBanner,
   },
 };
 </script>
