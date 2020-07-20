18175089beadf10b6b2dfbfe4e91672a7b428b5c
mazayastudio <mazayastudio@hotmail.com>
2020-07-20 18:58:45 +0700

    Hero Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="home">
     <HeaderShayna />
+    <HeroShayna />
   </div>
 </template>
 
 <script>
 import HeaderShayna from "./../components/HeaderShayna.vue";
+import HeroShayna from "./../components/HeroShayna.vue";
+
 export default {
   name: "Home",
   components: {
-    HeaderShayna
-  }
+    HeaderShayna,
+    HeroShayna,
+  },
 };
 </script>
